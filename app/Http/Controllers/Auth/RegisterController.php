418ba0d96fc2cdf55bd842d164ebd87317d33192
nikolaabadic <nikolaabadic@gmail.com>
2021-08-09 18:51:09 +0200

    Adding register, login, logout routes, middleware

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    public function __construct()
+    {
+        $this -> middleware(['guest']);
+    }
+
     public function index(){
         return view('auth.register');
     }
@@ -18,5 +25,16 @@ class RegisterController extends Controller
             'email' => 'required|max:255|email',
             'password' => 'required|confirmed'
         ]);
+
+        User::create([
+            'name' => $request->name,    
+            'email' => $request->email,   
+            'username' => $request->username,   
+            'password' => Hash::make($request->password)   
+        ]);
+
+        auth()->Auth::attempt($request->only('email', 'password'));
+
+        return redirect() -> route('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +16,19 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){
+    return view('home');
+}) -> name('home');
+Route::get('/dashboard', [DashboardController::class, 'index']) -> name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store']) -> name('logout');
 
 Route::get('/register', [RegisterController::class, 'index']) -> name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index']) -> name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
 
 Route::get('/posts', function () {
     return view('posts.index');
