6032a1ea9b8a250417fb769bcb46674c67188c06
nikolaabadic <nikolaabadic@gmail.com>
2021-08-10 16:37:41 +0200

    Delete post authorization

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::paginate(10);
+        $posts = Post::latest()->paginate(10);
         return view('posts.index', [
             'posts' => $posts
         ]);
@@ -23,6 +23,12 @@ class PostController extends Controller
             'body'=> $request-> body
         ]);
 
+        return back();
+    }
+
+    public function destroy(Post $post){
+        $this->authorize('delete', $post);
+        $post->delete();
         return back();
     }
 }
