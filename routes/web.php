d336469f3c5a1a89d54c50b5c9b9a50744e3233c
nikolaabadic <nikolaabadic@gmail.com>
2021-08-10 12:29:03 +0200

    Adding post create and get routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +30,6 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index']) -> name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
+Route::get('/posts', [PostController::class, 'index']) -> name('posts');
+Route::post('/posts', [PostController::class, 'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
