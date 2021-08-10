6d34f4ce88ec34e068400dea57c9088e670dd5f8
nikolaabadic <nikolaabadic@gmail.com>
2021-08-10 13:13:37 +0200

    Adding likes and unlikes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostLikeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,4 +33,7 @@ Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index']) -> name('posts');
 Route::post('/posts', [PostController::class, 'store']);
+
+Route::post('/posts/{post}/likes', [PostLikeController::class, 'store']) -> name('posts.likes');
+Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy']) -> name('posts.likes');
 
